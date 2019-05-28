57ba269f6bb49503cebf40cc4d4590c17d0ebe92
Alexeev Nickolay <zt50tz@gmail.com>
2019-05-28 23:32:20 +0300

    (fix?) setup

--- setup.py
+++ setup.py
@@ -1,14 +1,15 @@
 # coding: utf8
 
+from io import open
 from setuptools import setup
 
 from os import path
 this_directory = path.abspath(path.dirname(__file__))
-with open(path.join(this_directory, 'README.md')) as f:
-    long_description = f.read().decode('utf8')
+with open(path.join(this_directory, 'README.md'), encoding='utf-8') as f:
+    long_description = f.read()
 
 setup(name='yandex_connect',
-      version='0.26b',
+      version='0.27b',
       description='API Yandex Connect',
       url='http://github.com/zt50tz/yandex-connect',
       author='Alexeev Nick',
